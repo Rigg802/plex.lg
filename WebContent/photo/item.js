269b3f35b3ac226ab279dc8144f65489bad5df61
Simon J. Hogan <simon.hogan@iinet.net.au>
2013-12-04 20:08:31 +1100

    Moved javascript from html pages into seperate files

---
+++ WebContent/photo/item.js
@@ -0,0 +1,17 @@
+/**
+ * Javascript for photo/item.html
+ */
+
+var library = new Library();
+$(document).unload(library.showLoader("Loading ..."));
+				
+$(document).ready(function() {
+	var device = document.getElementById("device");
+	$(document).arrowNavigation();	
+	
+	library.showLoader("Loading ...");
+	library.getMediaItem($.querystring().key, function(xml) {
+		$("#application").append("<img src=\"" + library.getServerUrl() + "/photo/:/transcode?url=" + encodeURIComponent(library.getServerUrl() + $(xml).find("Part:first").attr("key")) + "&width=1260&height=700\"/>");
+		library.hideLoader();
+	});
+});
